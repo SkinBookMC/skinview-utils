ca87375e458f7b25c9aa8a53c2241773ff63908a
Naibuu <hawkwer141099@gmail.com>
2024-04-14 14:09:21 +0300

    fix: wings draw

--- src/process.ts
+++ src/process.ts
@@ -396,12 +396,12 @@ export function loadWingsToCanvas(
 	image: TextureSource
 ): void {
 	const scale = computeWingsScale(image);
-	canvas.width = 32 * scale;
-	canvas.height = 32 * scale;
+	canvas.width = 64 * scale;
+	canvas.height = 64 * scale;
 
 	const context = canvas.getContext("2d", {
 		willReadFrequently: true,
 	}) as CanvasContext;
 	context.clearRect(0, 0, canvas.width, canvas.height);
-	context.drawImage(image, 0, 11, image.width, image.height);
+	context.drawImage(image, 0, 0, image.width, image.height);
 }
